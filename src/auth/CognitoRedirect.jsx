536e092a4367f12c91a71156367f0e49ffa4f423
induwarafernando <induwarafernando9@gmail.com>
2025-09-05 02:59:13 +0530

    2.59

--- src/auth/CognitoRedirect.jsx
+++ src/auth/CognitoRedirect.jsx
@@ -85,10 +85,10 @@ export default function CognitoRedirect() {
   }
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-50 to-green-50">
-      <div className="bg-white shadow-lg rounded-2xl p-10 max-w-md w-full text-center">
+    <div className="min-h-screen flex items-center justify-center  bg-white  ">
+      <div className="bg-gradient-to-br from-blue-50 to-green-50 shadow-lg rounded-2xl p-10 max-w-md w-full text-center rounded-lg m-4">
         <div className="flex flex-col items-center mb-6">
-          <Stethoscope className="w-12 h-12 text-green-600 mb-3" />
+          <Stethoscope className="w-12 h-12 text-green-600 mb-3 mt-4" />
           <h1 className="text-3xl font-bold text-gray-800">MediSys Portal</h1>
           <p className="text-gray-500 text-sm mt-2">
             Secure access for clinics & internal staff
@@ -97,7 +97,16 @@ export default function CognitoRedirect() {
 
         <div className="space-y-4">
           <button
-            className="w-full px-6 py-3 bg-blue-600 text-white font-medium rounded-lg shadow hover:bg-blue-700 transition"
+            className="w-full/2 m-4 px-6 py-3 bg-green-500 text-white font-medium rounded-lg shadow hover:bg-green-800 transition"
+            onClick={() => {
+              window.location.href =
+                "https://us-east-1bvf8difqt.auth.us-east-1.amazoncognito.com/login?client_id=61hi982s20u919b8iij70i2d97&response_type=code&scope=email+openid+phone&redirect_uri=http://localhost:5173/internal";
+            }}
+          >
+            Login as Internal Staff
+          </button>
+          <button
+            className="w-full/2 m-4 px-6 py-3 bg-blue-500 text-white font-medium rounded-lg shadow hover:bg-blue-800 transition"
             onClick={() => {
               window.location.href =
                 "https://us-east-1gdpinciim.auth.us-east-1.amazoncognito.com/login?client_id=8q03mmehccquenkhikjog75it&redirect_uri=http://localhost:5173/dashboard/&response_type=code&scope=email+openid+phone";
@@ -106,15 +115,6 @@ export default function CognitoRedirect() {
             Login as Clinic User
           </button>
 
-          <button
-            className="w-full px-6 py-3 bg-green-600 text-white font-medium rounded-lg shadow hover:bg-green-700 transition"
-            onClick={() => {
-              window.location.href =
-                "https://us-east-1bvf8difqt.auth.us-east-1.amazoncognito.com/login?client_id=61hi982s20u919b8iij70i2d97&response_type=code&scope=email+openid+phone&redirect_uri=http://localhost:5173/internal";
-            }}
-          >
-            Login as Internal Staff
-          </button>
         </div>
       </div>
     </div>
